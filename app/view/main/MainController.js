789889268502659862c4f25b6531aed430762de1
Eugene Gordeiev <eugene@macbook.phub.net.cable.rogers.com>
2018-02-07 16:41:11 -0500

    changes

--- app/view/main/MainController.js
+++ app/view/main/MainController.js
@@ -5,9 +5,22 @@ Ext.define('MyApp.view.main.MainController', {
     extend: 'Ext.app.ViewController',
     alias: 'controller.main',
 
+    requires: [
+        'Test.view.MyMessageBox'
+    ],
+
     // Handles button click and displays the item in the message box
     itemClick: function(me, record, item, index, e, eOpts) {
-        Ext.Msg.alert('Activity ' + record.data.no, 'Date: ' + record.data.date + '<br/>User: ' + record.data.username);
+        //Ext.Msg.alert('Activity ' + record.data.no, 'Date: ' + record.data.date + '<br/>User: ' + record.data.username);
+        //Test.view.MyMessageBox.alert('Activity ' + record.data.no, 'Date: ' + record.data.date + '<br/>User: ' + record.data.username);
+        Ext.create('Test.view.MyMessageBox', {
+            header: {
+                title: 'Activity ' + record.data.no,
+                titleAlign: 'center'
+            },  
+
+			html: 'Date: ' + record.data.date + '<br/>User: ' + record.data.username 
+        }).show(); 
     },
 
     // Applies each record to the template and provides additional flexiblity if needed
